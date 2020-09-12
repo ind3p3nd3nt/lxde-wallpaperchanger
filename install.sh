3667b1e83066962f7d7b61d074221a249dc4ece5
Remi Girard <54126451+independentcod@users.noreply.github.com>
2020-09-12 03:10:37 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash -e
 apt install unzip wget -y;
-wget -O /root/molly.zip https://archive.org/download/molly_202009/molly.zip;
-unzip /root/molly.zip -d /root/Desktop/Wallpapers/;
+if [ ! -f "/root/molly.zip" ]; then wget -O /root/molly.zip https://archive.org/download/molly_202009/molly.zip; fi
+if [ -f "/root/molly.zip" ]; then unzip /root/molly.zip -d /root/Desktop/Wallpapers/; fi
 chmod +x lxde-wallpaperchanger/wallpaperchanger.sh;
 ./lxde-wallpaperchanger/wallpaperchanger.sh&
 exit 0
